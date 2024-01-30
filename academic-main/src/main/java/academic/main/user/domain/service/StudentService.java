eb452683d81888db7f61cb61d33658380104c126
박종빈 <runnz121@gmail.com>
2024-01-30 23:59:02 +0900

    [feat] 누락된 서비스 어노테이션 추가

--- academic-main/src/main/java/academic/main/user/domain/service/StudentService.java
+++ academic-main/src/main/java/academic/main/user/domain/service/StudentService.java
@@ -3,10 +3,12 @@ package academic.main.user.domain.service;
 import academic.main.user.domain.entity.Students;
 import academic.main.user.domain.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Service
 public class StudentService {
 
     private final StudentRepository studentRepository;
